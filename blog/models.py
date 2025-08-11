415166b8207a6114e7f7887cbd32633e3d873173
Another-User-Dev <michael.jobsearch.emails@gmail.com>
2025-08-11 23:26:56 +0100

    Debug model before migration

--- blog/models.py
+++ blog/models.py
@@ -17,7 +17,6 @@ class Post(models.Model):
     updated_on = models.DateTimeField(auto_now=True)
     
 class Comment(models.model):
-    author = models.ForeignKey(
     post = models.ForeignKey(
         Post, on_delete=models.CASCADE, related_name="comments"
     )
